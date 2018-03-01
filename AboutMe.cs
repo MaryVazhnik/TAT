c202ad2ae2a9f5794117907784cc341cee4c6a84
mary.vazhnik <donmarino79@gmail.com>
2018-03-02 00:28:14 +0200

    This message about me

---
+++ AboutMe.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AboutMe
+{
+    //This class is for displaying message about me
+    class Name
+    {
+        // Entry point
+        static void Main()
+        {
+            Console.WriteLine("My name is Maryia Vazhnik, I am student of BSU, Faculty of Radiophysics and Computer Technologies");
+            Console.ReadKey();
+        }
+    }
+}
