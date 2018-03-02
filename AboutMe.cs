ea933fb0d1e3737e0ed2ba9100ab8d0479863223
mary.vazhnik <donmarino79@gmail.com>
2018-03-02 13:15:06 +0200

    Download AboutMe

--- AboutMe.cs
+++ AboutMe.cs
@@ -2,10 +2,14 @@ using System;
 
 namespace AboutMe
 {
+    /// <summary>
     //This class is for displaying message about me
+    /// </summary>
     class Name
     {
+        /// </summary>
         // Entry point
+        /// </summary>
         static void Main()
         {
             Console.WriteLine("My name is Maryia Vazhnik, I am student of BSU, Faculty of Radiophysics and Computer Technologies");
