b60b7827a8afa6cfe76584152469b994590243d2
mary.vazhnik <donmarino79@gmail.com>
2018-03-04 14:07:36 +0200

    Upload task.

---
+++ DEV-2/DEV_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Managing general information about the assembly is done using
+// set of attributes. Change the values ​​of these attributes to change the information,
+// associated with the assembly.
+[assembly: AssemblyTitle("DEV_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DEV_2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The ComVisible parameter with the FALSE value makes the types in the assembly invisible
+// for COM components. If you want to access the type in this assembly via
+// COM, set the ComVisible attribute to TRUE for this type.
+[assembly: ComVisible(false)]
+
+// The following GUID is used to identify the type library if this project is visible to COM
+[assembly: Guid("4c3308cb-d004-41ce-8229-11475a17c256")]
+
+// The version information for the assembly consists of the following four values:
+//
+// Main version number
+// Additional version number
+// Build number
+// Revision
+//
+// You can set all values ​​or accept assembly numbers and revision numbers by default
+// using "*", as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DEV-2/DEV_2/Selection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    /// <summary>
+    ///Class for deleting all odd symbols 
+    /// </summary>
+    class Selection
+    {
+        /// <summary>
+        /// Entry Point.
+        /// </summary>
+        static void Main(string[] args)
+        {
+           StringBuilder sb = new StringBuilder();
+           Console.WriteLine("Write line");
+           sb.Append(Console.ReadLine());
+           for (int i = 0; i < sb.Length; i++)
+           {
+              sb = sb.Remove(i, 1);
+           }
+           Console.WriteLine("Symbols with even indices: "+sb);
+        }
+    }
+}
