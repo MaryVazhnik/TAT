9eb1141fafc4e9a3db98e917e70ec43e455a4010
MaryVazhnik <36933552+MaryVazhnik@users.noreply.github.com>
2018-03-07 20:16:16 +0200

    Update ConverterOfNumeralSystems.cs

--- DEV_3/DEV_3/ConverterOfNumeralSystems.cs
+++ DEV_3/DEV_3/ConverterOfNumeralSystems.cs
@@ -14,7 +14,8 @@ namespace DEV_3
             Console.WriteLine("Number " + args[0] + " in " + args[1] + " number system:");
             CalculationMethod prog = new CalculationMethod();
             int check = Int32.Parse(args[1]);
-            if (check <= 20 && check >= 2)
+            int args2 = Int32.Parse(args[0]);
+            if (check <= 20 && check >= 2 && args2 > 0)
             {
                 prog.ToNumberSystem(args[0], args[1]);
             }
