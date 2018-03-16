d9f4e437c049a9faddfe19a23007188498ccfaf8
MaryVazhnik <36933552+MaryVazhnik@users.noreply.github.com>
2018-03-16 10:12:22 +0300

    Add files with tests

---
+++ SequenceBuilder.Test/SequenceBuilderTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DEV_2;
+using Xunit;
+
+namespace Sequence.Test
+{
+    public class SequenceBuilderTest
+    {
+        [Fact]
+        public void EmptyStringTest()
+        {
+            // arrange
+            string str = String.Empty;
+            //act
+            SequenceBuilder sb = new SequenceBuilder();
+            //assert
+            Assert.Null(sb.CheckEven(str));
+        }
+        [Fact]
+        public void EvenIndexTest()
+        {
+            //arrange
+            string str = "ababab";
+            string result = "aaa";
+            //act
+            SequenceBuilder sb = new SequenceBuilder();
+            //assert
+            Assert.Equal(sb.CheckEven(str), result);
+        }
+    }
+}
