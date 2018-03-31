ce44355dc2913d8904a076af20e2ffe20bd50c6a
MaryVazhnik <36933552+MaryVazhnik@users.noreply.github.com>
2018-03-31 11:29:16 +0300

    Uploaded files

---
+++ DEV_7/ParseJson.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_7
+{
+    class ParseJson
+    {
+        public static List<Car> list = new List<Car>();
+        public static List<Car> serialize()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            FileStream s = File.Open("cars.json", FileMode.Open);
+            StreamReader sr = new StreamReader(s);
+            JsonReader reader = new JsonTextReader(sr);
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    list.Add(serializer.Deserialize<Car>(reader));
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ DEV_7/Search.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_7
+{
+    public class Search
+    {
+        public static void switchCase()
+        {
+            Console.WriteLine("Which part of your future car you want enter: ");
+            Console.WriteLine("1: Body | (Coupe, Sedan, Minivan, Wagon)");
+            Console.WriteLine("2: Cabin | (Leather, Fabric, Combined)");
+            Console.WriteLine("3: Engine | (Gasoline, Diesel, Electric)");
+            Console.WriteLine("4: ClimateManagment | (ClimateControle, Conditioner, Null)");
+            Console.WriteLine("5: Transmission | (Automatic, Manual)");
+            Console.WriteLine("6: Brand");
+            Console.WriteLine("7: Model");
+            Console.WriteLine("8: Capacity");
+            Console.WriteLine("9: Power");
+            Console.WriteLine("10: All cars");
+            Console.WriteLine("11: Purchase of car");
+            String part = Console.ReadLine();
+            Console.Write("Enter variable: ");
+            switch (part)
+            {
+                case "1": CreateCriteria.checkBody(Console.ReadLine());
+                    break;
+                case "2": CreateCriteria.checkCabin(Console.ReadLine());
+                    break;
+                case "3": CreateCriteria.checkEngine(Console.ReadLine());
+                    break;
+                case "4": CreateCriteria.checkClimateManagment(Console.ReadLine());
+                    break;
+                case "5": CreateCriteria.checkTransmission(Console.ReadLine());
+                    break;
+                case "6": CreateCriteria.checkBrand(Console.ReadLine());
+                    break;
+                case "7": CreateCriteria.checkModel(Console.ReadLine());
+                    break;
+                case "8": CreateCriteria.checkCapacity(Console.ReadLine());
+                    break;
+                case "9": CreateCriteria.checkPower(Console.ReadLine());
+                    break;
+                case "10": CreateCriteria.checkNull();
+                    break;
+                case "11": CreateCriteria.toJson();
+                    break;
+                default: Console.WriteLine("Error! Try again");
+                    switchCase();
+                    break;
+            }
+        }
+    }
+}
