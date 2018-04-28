a36a44ad7d196dcb1457b8a82375065247f5b715
mary.vazhnik <donmarino79@gmail.com>
2018-04-28 19:35:42 +0200

    Add files

---
+++ DEV-6/DEV-6/Goods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_6
+{
+    class Goods
+    {
+        public String type { get; set; }
+        public String name { get; set; }
+        public int quantity { get; set; }
+        public double costPerUnit { get; set; }
+
+        public Goods() {}
+        public Goods(String type, String name, int quantity, double costPerUnit)
+        {
+            this.type = type;
+            this.name = name;
+            this.quantity = quantity;
+            this.costPerUnit = costPerUnit;
+        }
+    }
+}
