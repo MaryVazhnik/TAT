0cd1017ae52b1edb14cfbd768b33234a850e62f9
mary.vazhnik <donmarino79@gmail.com>
2018-05-21 21:57:08 +0200

    New files

---
+++ ControlWork/Task_2/AreaOfRectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class AreaOfRectangle
+    {
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public AreaOfRectangle(int width, int length)
+        {
+            Width = width;
+            Length = length;
+        }
+        public int Calculate(int width, int length)
+        {
+            int area = width * length;
+            return area;
+        }
+    }
+}
---
+++ ControlWork/Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+          { 
+                int length = int.Parse(args[0]); 
+                int width = int.Parse(args[1]); 
+            try 
+            { 
+                if (length < 0 && width < 0 ) 
+                    { 
+                        Console.WriteLine("Negative value."); 
+                        return; 
+                    } 
+                if (length > int.MaxValue && width > int.MaxValue) 
+                    { 
+                        Console.WriteLine("Much value."); 
+                        return; 
+                    }
+                AreaOfRectangle calc = new AreaOfRectangle(width, length);
+                calc.Calculate(width, length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          }
+    }
+}
