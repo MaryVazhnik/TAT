a894c4a314273df1c5a938494e99e76720ea9643
mary.vazhnik <donmarino79@gmail.com>
2018-05-21 20:13:51 +0200

    New files

---
+++ ControlWork/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("And hi again!");
+            Random rand = new Random();
+            int size = rand.Next(5, 50);
+            for (int i = 5; i < size; i++)
+            {
+                Console.Write("!");
+            }
+        }
+    }
+}
