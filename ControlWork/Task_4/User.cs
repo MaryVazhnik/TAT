9df6e64e9ed1d379ebf2a904fe4a56ef5d4f4b2d
mary.vazhnik <donmarino79@gmail.com>
2018-05-21 21:41:28 +0200

    New files

---
+++ ControlWork/Task_4/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public User() { }
+        public User(string Name, string Surname, int Age)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine(this.Name + " " + this.Surname + " " + this.Age);
+        }
+    }
+}
---
+++ ControlWork/Task_4/UserBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class UserBuilder
+    {
+        private User user;
+        public UserBuilder()
+        {
+            user = new User();
+        }
+        public void SetFirstName(string name)
+        {
+            if (name.Equals(string.Empty))
+            {
+                throw new ArgumentException();
+            }
+            user.Name = name;
+        }
+        public void SetLastName(string lastName)
+        {
+            if (lastName.Equals(string.Empty))
+            {
+                throw new ArgumentException();
+            }
+            user.Surname = lastName;
+        }
+        public void SetAge(string age)
+        {
+            user.Age = Int32.Parse(age);
+        }
+        public User Build()
+        {
+            return user;
+        }
+    }
+}
